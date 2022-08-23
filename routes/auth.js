1c611405486afdfbbe9b090ddf7b7a079804947d
MSI\laptop msi <lauraq2348@gmail.com>
2022-08-23 13:33:51 -0400

    added role validations to create users

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const { validations } = require("../middlewares/validations");
+const { AdminRole } = require("../middlewares/role-validation");
 const UserController = require("../controllers/UserController");
 const validateSignup = require("../validators/validateSignup");
 const validateLogin = require("../validators/validateLogin");
@@ -7,8 +8,11 @@ const validateLogin = require("../validators/validateLogin");
 // base path: api/users
 const router = Router();
 
+// check for role
+const checkAdminRole = () => [AdminRole];
+
 // write operations
-router.post("/signup", validateSignup, UserController.post);
+router.post("/signup", checkAdminRole(), validateSignup, UserController.post);
 router.post("/login", validateLogin, UserController.login);
 
 router.use(validations);
