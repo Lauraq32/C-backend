68c1c47582bf9c2fe81307cb429e42c84a5222b0
MSI\laptop msi <lauraq2348@gmail.com>
2022-08-24 19:05:02 -0400

    fixed mistake

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,5 @@
 const { Router } = require("express");
 const { validations } = require("../middlewares/validations");
-const { AdminRole } = require("../middlewares/role-validation");
 const UserController = require("../controllers/UserController");
 const validateSignup = require("../validators/validateSignup");
 const validateLogin = require("../validators/validateLogin");
@@ -8,11 +7,8 @@ const validateLogin = require("../validators/validateLogin");
 // base path: api/users
 const router = Router();
 
-// check for role
-const checkAdminRole = () => [AdminRole];
-
 // write operations
-router.post("/signup", checkAdminRole(), validateSignup, UserController.post);
+router.post("/signup", validateSignup, UserController.post);
 router.post("/login", validateLogin, UserController.login);
 
 router.use(validations);
